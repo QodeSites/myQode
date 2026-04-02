b7e939e77d269abd4fff6185733527b713916ec8
001AM <sohampanchal1469@gmail.com>
2026-04-02 17:59:19 +0530

    remove created at in admin login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -111,7 +111,6 @@ export async function POST(request: NextRequest) {
         { status: 401 }
       )
     }
-    console.log(user.email, "=========email")
 
     await query(
       `UPDATE pms_clients_master 
